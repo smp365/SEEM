612579710a46c8eed1c5caf9f4ea3b8eb69a3cd7
smp365 <131334649+smp365@users.noreply.github.com>
2023-05-20 14:45:06 -0700

    Update combine_masks.py

--- combine_masks.py
+++ combine_masks.py
@@ -12,6 +12,6 @@ def combine_masks(mask_files):
 
 # usage
 # mask_files = ["object1_mask.png", "object2_mask.png", "object3_mask.png"]
-mask_files = ["1_mask.png", "3_mask.png", "4_mask.png", "5_mask.png", "7_mask.png", "15_mask.png", "17_mask.png", "21_mask.png", "22_mask.png", 24_mask.png", 26_mask.png", 27_mask.png"]
+mask_files = ["1_mask.png", "3_mask.png", "4_mask.png", "5_mask.png", "7_mask.png", "15_mask.png", "17_mask.png", "21_mask.png", "22_mask.png", "24_mask.png", "26_mask.png", "27_mask.png"]
 combined_mask = combine_masks(mask_files)
 cv2.imwrite("combined_mask.png", combined_mask)
